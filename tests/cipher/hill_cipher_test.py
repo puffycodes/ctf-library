ab9254c86d3caf6b69a726e76e1324aa287c4ef8
puffycodes <as1234dfgh@gmail.com>
2024-06-16 21:17:12 +0800

    move hill cipher key to a separate class

--- tests/cipher/hill_cipher_test.py
+++ tests/cipher/hill_cipher_test.py
@@ -49,15 +49,13 @@ class HillCipherTest(unittest.TestCase):
             key = HillCipher.HillCipherKey(key_data)
         except ValueError as e:
             print(f'ValueError: {e}')
-            print('-----***--')
+            print('=====***==')
             return
         
         cipher = HillCipher(key)
-        # print(f'key:\n{cipher.key}')
-        # print(f'key inverse:\n{cipher.key_inv}')
         print(f'encryption matrix:\n{cipher.key.encryption_matrix}')
         print(f'decryption matrix:\n{cipher.key.decryption_matrix}')
-        print('=====')
+        print('-----')
 
         cipher_text = cipher.encrypt(plain_text)
         decrypted_text = cipher.decrypt(cipher_text)
