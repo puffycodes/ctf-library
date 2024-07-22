6c37d4d1f3dc427d524e5b18fd92858f1d315344
puffycodes <as1234dfgh@gmail.com>
2024-07-22 22:43:46 +0800

    add parser for png files

--- ctf_library/file_format/pngfile_format.py
+++ ctf_library/file_format/pngfile_format.py
@@ -12,6 +12,7 @@ class PNGFileFormat:
 
     PNGHeader = b'\x89\x50\x4e\x47\x0d\x0a\x1a\x0a'
 
+    # TODO: Do we need both offset and pos?
     @staticmethod
     def parse(data, offset=0, max_length=-1, pos=0, with_header=True):
         data_length = len(data) - pos - offset
