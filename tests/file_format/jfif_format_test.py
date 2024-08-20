929ce648e1cdbf98a474c8dd229aa8ef2758cee1
puffycodes <as1234dfgh@gmail.com>
2024-08-20 16:20:33 +0800

    add tests for jfif file format

---
+++ tests/file_format/jfif_format_test.py
@@ -0,0 +1,17 @@
+# file: jfif_format_test.py
+
+import unittest
+from ctf_library.file_format.jfif_format import JFIFFileFormat
+
+class JFIFFileFormatTest(unittest.TestCase):
+
+    data_file_dir = 'data/jfiffile'
+
+    def test_01(self):
+        print(f'empty test')
+        return
+    
+if __name__ == '__main__':
+    unittest.main()
+
+# --- end of file --- #
