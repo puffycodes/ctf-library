ce9d42c91e13b1b8be734ac4c92b0281c4bc8742
puffycodes <as1234dfgh@gmail.com>
2024-08-27 15:22:23 +0800

    add gcd and lcm functions

--- tests/math/mathlib_test.py
+++ tests/math/mathlib_test.py
@@ -16,52 +16,31 @@ class MathLibTest(unittest.TestCase):
 
     def test_gcd(self):
         verbose = False
-        # test_cases = [
-        #     [ 1, 1, 1 ], [ 5, 7, 1 ], [ 35, 7, 7 ],
-        #     [ -5, 7, 1 ], [ 5, -7, -1 ], [ -5, -7, -1 ],
-        #     [ -1, 1, 1 ], [ 1, -1, -1 ], [ -1, -1, -1 ],
-        #     [ 0, 7, 7 ], [ 7, 0, 7 ], [ 0, 0, 0 ],
-        #     [ 210, 30, 30 ], [ 210, 150, 30 ], [ 1000, 50, 50 ],
-        # ]
-        for a, b, expected, _ in MathLibTest.test_cases:
+        for a, b, gcd_expected, _ in MathLibTest.test_cases:
             g = MathLib.gcd(a, b)
             if verbose:
                 print(f'gcd({a}, {b}) = {g}')
-            self.assertEqual(g, expected)
+            self.assertEqual(g, gcd_expected)
         return
     
     def test_xgcd(self):
         verbose = False
-        # test_cases = [
-        #     [ 1, 1, 1 ], [ 5, 7, 1 ], [ 35, 7, 7 ],
-        #     [ -5, 7, 1 ], [ 5, -7, -1 ], [ -5, -7, -1 ],
-        #     [ -1, 1, 1 ], [ 1, -1, -1 ], [ -1, -1, -1 ],
-        #     [ 0, 7, 7 ], [ 7, 0, 7 ], [ 0, 0, 0 ],
-        #     [ 210, 30, 30 ], [ 210, 150, 30 ], [ 1000, 50, 50 ],
-        # ]
-        for a, b, expected, _ in MathLibTest.test_cases:
+        for a, b, gcd_expected, _ in MathLibTest.test_cases:
             g, x, y = MathLib.xgcd(a, b)
             if verbose:
                 print(f'gcd({a}, {b}) = {g} = ({a})*({x}) + ({b})* ({y})')
-            self.assertEqual(g, expected)
+            self.assertEqual(g, gcd_expected)
             g_computed = a * x + b * y
             self.assertEqual(g_computed, g)
         return
     
     def test_lcm(self):
         verbose = False
-        # test_cases = [
-        #     [ 1, 1, 1 ], [ 5, 7, 35 ], [ 35, 7, 35 ],
-        #     [ -5, 7, 35 ], [ 5, -7, 35 ], [ -5, -7, 35 ],
-        #     [ -1, 1, 1 ], [ 1, -1, 1 ], [ -1, -1, 1 ],
-        #     [ 0, 7, 0 ], [ 7, 0, 0 ], [ 0, 0, 0 ],
-        #     [ 210, 30, 210 ], [ 210, 150, 1050 ], [ 1000, 50, 1000 ],
-        # ]
-        for a, b, _, expected in MathLibTest.test_cases:
+        for a, b, _, lcd_expected in MathLibTest.test_cases:
             l = MathLib.lcm(a, b)
             if verbose:
                 print(f'lcm({a}, {b}) = {l}')
-            self.assertEqual(l, expected)
+            self.assertEqual(l, lcd_expected)
         return
     
 if __name__ == '__main__':
