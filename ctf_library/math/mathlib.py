53aa9543ae3fc3adcac4c59692b8c05e96e724b0
puffycodes <as1234dfgh@gmail.com>
2024-08-28 16:48:02 +0800

    add gcd and lcm functions

--- ctf_library/math/mathlib.py
+++ ctf_library/math/mathlib.py
@@ -13,8 +13,8 @@
 # - Ref: https://www.cuemath.com/numbers/lcm-least-common-multiple/
 
 # Euclidean Algorithms
-# gcd() Ref: https://en.wikipedia.org/wiki/Euclidean_algorithm
-# xgcd() Ref: https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
+# - gcd() Ref: https://en.wikipedia.org/wiki/Euclidean_algorithm
+# - xgcd() Ref: https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
 
 class MathLib:
 
