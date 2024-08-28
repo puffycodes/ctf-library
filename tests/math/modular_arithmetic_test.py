be95977fc8b6b2f0c4be11411620ca78dd22792b
puffycodes <as1234dfgh@gmail.com>
2024-08-28 16:18:21 +0800

    add modular arithmetic

--- tests/math/modular_arithmetic_test.py
+++ tests/math/modular_arithmetic_test.py
@@ -1,10 +1,13 @@
 # file: modular_arithmetic.py
 
+from ast import mod
 import unittest
 import random
 from ctf_library.math.modular_arithmetic import ModularArithmetic
 
 class ModularArithmeticTest(unittest.TestCase):
+
+    # --- Test Cases
 
     def test_multiplicative_inverse(self):
         verbose = False
@@ -25,12 +28,25 @@ class ModularArithmeticTest(unittest.TestCase):
         return
     
     def test_mod_sqrt(self):
-        verbose = True
+        verbose = False
         modulo_list = [ 17, 32, 37, 55, 59, 199, 293, 991, 997 ]
         for modulo in modulo_list:
             for number in range(modulo):
                 self.do_check_mod_sqrt_tonelli_shanks(number, modulo, verbose=verbose)
+        for modulo in modulo_list:
+            for number in range(modulo):
+                self.do_check_mod_sqrt_slow(number, modulo, verbose=verbose)
         return
+    
+    def test_mod_sqrt_compare(self):
+        verbose = False
+        modulo_list = [ 17, 32, 55, 997 ]
+        for modulo in modulo_list:
+            for number in range(modulo):
+                self.do_compare_mod_sqrt(number, modulo, verbose=verbose)
+        return
+    
+    # --- Internal Functions
     
     def do_check_multiplicative_inverse_random(self, rounds=100,
                                                start_n=2, end_n=9999999999,
@@ -55,8 +71,27 @@ class ModularArithmeticTest(unittest.TestCase):
         return
     
     def do_check_mod_sqrt_tonelli_shanks(self, number, modulo, verbose=False):
+        self.do_check_mod_sqrt_function(
+            number, modulo,
+            mod_sqrt_ftn=ModularArithmetic.mod_sqrt_tonelli_shanks,
+            verbose=verbose
+        )
+        return
+    
+    def do_check_mod_sqrt_slow(self, number, modulo, verbose=False):
+        self.do_check_mod_sqrt_function(
+            number, modulo,
+            mod_sqrt_ftn=ModularArithmetic.mod_sqrt_slow,
+            verbose=verbose
+        )
+        return
+    
+    def do_check_mod_sqrt_function(self,
+                                   number, modulo, 
+                                   mod_sqrt_ftn=ModularArithmetic.mod_sqrt_tonelli_shanks,
+                                   verbose=False):
         try:
-            sqrt_list = ModularArithmetic.mod_sqrt_tonelli_shanks(number, modulo)
+            sqrt_list = mod_sqrt_ftn(number, modulo)
             if verbose:
                 print(f'mod_sqrt({number}, {modulo}) = {sqrt_list}')
             for sqrt in sqrt_list:
@@ -65,6 +100,18 @@ class ModularArithmeticTest(unittest.TestCase):
         except ValueError as e:
             if verbose:
                 print(f'no modular square root for {number}, modulo {modulo}')
+        return
+    
+    def do_compare_mod_sqrt(self, number, modulo, verbose=False):
+        try:
+            sqrt_list_1 = ModularArithmetic.mod_sqrt_tonelli_shanks(number, modulo)
+        except ValueError as e:
+            sqrt_list_1 = []
+            if verbose:
+                print(f'no modular square root for {number}, modulo {modulo}')
+        sqrt_list_2 = ModularArithmetic.mod_sqrt_slow(number, modulo)
+        if verbose or len(sqrt_list_1) != len(sqrt_list_2):
+            print(f'mod_sqrt({number}, {modulo}) = {sqrt_list_1}, {sqrt_list_2}')
         return
     
 if __name__ == '__main__':
