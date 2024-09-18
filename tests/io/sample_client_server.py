fe9a053a8427262a78a8e5dbb2fd02041eb66403
puffycodes <as1234dfgh@gmail.com>
2024-09-18 15:57:40 +0800

    add client and server wrappers for telnetlib3

--- tests/io/sample_client_server.py
+++ tests/io/sample_client_server.py
@@ -5,67 +5,71 @@ import argparse
 import asyncio
 from ctf_library.io.remote_connection import RemoteConnection
 
-host = 'localhost'
-port = 8000
+default_host = 'localhost'
+default_port = 8000
 
-async def server_shell(reader, writer):
-    writer.write('What do you have to say? ')
-    inp = await reader.readline()
-    if inp:
-        print(f'client says this: {inp}', end='')
-        writer.write(f'You said this: {inp}')
-        writer.write(f'It is not easy to do echo.')
-        await writer.drain()
-    writer.close()
-    return
+class ClientServerDemoShell:
 
-client_responses = [
-    'I am good.',
-    'I like python but I don\'t like snakes.',
-    'The quick brown fox jumps over the lazy dog.',
-]
+    def __init__(self):
+        self.client_responses = [
+            'I am good.',
+            'I like python but I don\'t like snakes.',
+            'The quick brown fox jumps over the lazy dog.',
+        ]
+        self.client_send_random_response = False
+        return
 
-client_send_random_response = False
+    async def server_shell(self, reader, writer):
+        writer.write('What do you have to say? ')
+        inp = await reader.readline()
+        if inp:
+            print(f'client says this: {inp}', end='')
+            writer.write(f'You said this: {inp}')
+            writer.write(f'It is not easy to do echo.')
+            await writer.drain()
+        writer.close()
+        return
 
-async def client_shell(reader, writer):
-    # read something
-    inp = await reader.readuntil(separator=b'? ')
-    if not inp:
-        # end of file
-        print(f'EOF', end='')
-    else:
-        print(f'{inp.decode()}', end='')
-        # response with something
-        if client_send_random_response:
-            reply = random.choice(client_responses) + '\n'
-            print(f'{reply}', end='')
-        else:
-            reply = input() + '\n'
-        writer.write(reply)
-        # wait for reply
-        inp = await reader.read()
+    async def client_shell(self, reader, writer):
+        # read something
+        inp = await reader.readuntil(separator=b'? ')
         if not inp:
             # end of file
             print(f'EOF', end='')
         else:
-            print(f'{inp}', end='')
-    print()
-    return
+            print(f'{inp.decode()}', end='')
+            # response with something
+            if self.client_send_random_response:
+                reply = random.choice(self.client_responses) + '\n'
+                print(f'{reply}', end='')
+            else:
+                reply = input() + '\n'
+            writer.write(reply)
+            # wait for reply
+            inp = await reader.read()
+            if not inp:
+                # end of file
+                print(f'EOF', end='')
+            else:
+                print(f'{inp}', end='')
+        print()
+        return
 
 def start_server(port):
     '''
     Start a server
     '''
-    RemoteConnection.create_server(port, server_shell)
+    shell = ClientServerDemoShell()
+    RemoteConnection.create_server(port, shell.server_shell)
     return
 
 def start_client(host, port, send_random_response=False):
     '''
     Start a client
     '''
-    global client_send_random_response
-    client_send_random_response = send_random_response
-    RemoteConnection.open_connection(host, port, client_shell)
+    shell = ClientServerDemoShell()
+    shell.client_send_random_response = send_random_response
+    RemoteConnection.open_connection(host, port, shell.client_shell)
     return
 
 async def start_client_and_server_async(host, port, rounds=10, terminate=True):
@@ -73,15 +77,15 @@ async def start_client_and_server_async(host, port, rounds=10, terminate=True):
     A demo on how to start both a client and a server in the
     same process
     '''
-    global client_send_random_response
-    client_send_random_response = True
+    shell = ClientServerDemoShell()
+    shell.client_send_random_response = True
     server_task = asyncio.create_task(
-        RemoteConnection.create_server_async(port, server_shell)
+        RemoteConnection.create_server_async(port, shell.server_shell)
     )
     for round in range(rounds):
         print(f'round {round+1}:')
         await RemoteConnection.open_connection_async(
-            host, port, client_shell
+            host, port, shell.client_shell
         )
         await asyncio.sleep(2)
     if not terminate:
@@ -98,22 +102,26 @@ def main():
         prog='client-server-demo',
         description='a client and server demo'
     )
-    parser.add_argument('--demo', action='store_true', default=False,
-                        help='run a demo with both client and server')
+    parser.add_argument('--host', default=default_host,
+                        help='name of host')
+    parser.add_argument('--port', default=default_port, type=int,
+                        help='port number')
     parser.add_argument('-s', '--server', action='store_true',
                         default=False,
                         help='run as a server (default is to run as a client)')
     parser.add_argument('-r', '--random', action='store_true',
                         default=False,
-                        help='send a random response (only applicable to client)')
+                        help='send a random message (only applicable to client)')
+    parser.add_argument('--demo', action='store_true', default=False,
+                        help='run a demo with both client and server')
     args = parser.parse_args()
     if args.demo:
-        start_client_and_server(host, port)
+        start_client_and_server(args.host, args.port)
     else:
         if args.server:
-            start_server(port)
+            start_server(args.port)
         else:
-            start_client(host, port, send_random_response=args.random)
+            start_client(args.host, args.port, send_random_response=args.random)
     return
 
 if __name__ == '__main__':
